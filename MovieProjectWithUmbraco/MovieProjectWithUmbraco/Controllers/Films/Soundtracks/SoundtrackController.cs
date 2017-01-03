e9dfec4f0335f0eb04ef10e64be0d75f1d2926a3
Max Konyukh <maximkonyh10@gmail.com>
2017-01-03 20:21:18 +0200

    Added ability to see soundtracks on page

---
+++ MovieProjectWithUmbraco/MovieProjectWithUmbraco/Controllers/Films/Soundtracks/SoundtrackController.cs
@@ -0,0 +1,70 @@
+﻿using MovieProjectWithUmbraco.Models;
+using Newtonsoft.Json.Linq;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Mvc;
+using Umbraco.Core.Models;
+using Umbraco.Web;
+using Umbraco.Web.Models;
+using Umbraco.Web.Mvc;
+
+namespace MovieProjectWithUmbraco.Controllers.Films.Soundtracks
+{
+    public class SoundtrackController : SurfaceController
+    {
+        const string PATH_TO_SOUNDTRACKS_FOLDER = "~/Views/Partials/Film/Soundtrack/";
+
+        public ActionResult RenderSoundtracksList()
+        {
+            var soundtrackList = GetSoundtrackList();
+
+            return PartialView(PATH_TO_SOUNDTRACKS_FOLDER + "_SoundtrackList.cshtml", soundtrackList);
+        }
+
+        private SoundtracksList GetSoundtrackList()
+        {
+            var soundtrackList = CurrentPage.Children.Where(d => d.DocumentTypeAlias == "soundtracksList").FirstOrDefault();
+
+            return new SoundtracksList
+            {
+                ImagePath = soundtrackList.GetCropUrl(propertyAlias: "image", imageCropMode: ImageCropMode.Max),
+                Title = soundtrackList.GetPropertyValue<string>("title"),
+                TotalDuration = soundtrackList.GetPropertyValue<int>("totalDuration"),
+                Soundtracks = GetSoundtracks(soundtrackList).ToList()
+            };
+        }
+
+        private IEnumerable<Soundtrack> GetSoundtracks(IPublishedContent page)
+        {
+            foreach (var soundtrack in page.Children)
+            {
+                yield return new Soundtrack
+                {
+                    Title = soundtrack.GetPropertyValue<string>("title"),
+                    Composer = GetLinkResponses(soundtrack, "composer"),
+                    Duration = soundtrack.GetPropertyValue<int>("duration")
+                };
+            }
+        }
+
+        private IEnumerable<LinkResponse> GetLinkResponses(IPublishedContent page, string alias)
+        {
+            if (page.HasValue(alias))
+            {
+                foreach (var item in page.GetPropertyValue<JArray>(alias))
+                {
+                    var linkCaption = item.Value<string>("caption");
+                    var linkUrl = (item.Value<bool>("isInternal")) ? Umbraco.NiceUrl(item.Value<int>("internal")) : item.Value<string>("link");
+                    var linkTarget = item.Value<bool>("newWindow") ? "_blank" : null;
+
+                    yield return new LinkResponse
+                    {
+                        Caption = linkCaption,
+                        Url = linkUrl,
+                        Target = linkTarget
+                    };
+                }
+            }
+        }
+    }
+}
---
+++ MovieProjectWithUmbraco/MovieProjectWithUmbraco/Models/FilmModels/SoundtrackModels/SoundtracksList.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+namespace MovieProjectWithUmbraco.Models
+{
+    public class SoundtracksList
+    {
+        public string Title { get; set; }
+        public string ImagePath { get; set; }
+        public int TotalDuration { get; set; }
+        public IEnumerable<Soundtrack> Soundtracks { get; set; }
+    }
+}
